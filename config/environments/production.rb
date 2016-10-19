a2c25931da308a813fcfdd3a69a29f673049994d
alex shyaka <shyakaster@gmail.com>
2016-10-19 09:54:51 +0300

    add authentication with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Devise set up
+  # required for devise gem but remember to change localhost:3000 to the live url
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'codingshop.herokuapp.com',:protocol =>'https' }
 end
